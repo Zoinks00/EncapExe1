3e3e5c5aaa61e840eb2b5b7ae69da7425bc71d88
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-12-26 14:45:45 -0800

    1st Push

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+   public  class BankAccount
+    {
+        
+        private double _balance = 0;
+               
+      public void Deposit(double value)
+        {
+            Console.WriteLine($"You have made a deposit of {value,0:c} to your account.");
+            _balance = value;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+      
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bAccount = new BankAccount();
+            Console.WriteLine($"Please enter the amount you wish to deposit.");
+            double addDeposit = double.Parse(Console.ReadLine());
+
+            bAccount.Deposit(addDeposit);
+            bAccount.GetBalance();
+
+        }
+    }
+}
